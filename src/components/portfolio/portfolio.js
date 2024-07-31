3680d3b5882155a46d5c99d867ddeee4ee3f9fad
Banukaiw <banukaiw@gmail.com>
2024-07-31 22:54:22 +0530

    errorfix

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -31,6 +31,20 @@ const Portfolio = () => {
           </butoon>
         </div> */}
 
+         {/* <div class="services-list2">
+          <div className="work1"></div>
+          <div className="work2"></div>
+          <div className="work3"></div>
+          <div className="work4"></div>
+          <div className="work6"></div>
+          <div className="work5"></div>
+        </div>
+        <div className="portbtn1">
+          <butoon className="portbtn" id="loadmore">
+            Load More <i class="fa-solid fa-angle-down"></i>
+          </butoon>
+        </div> */}
+
 
 
         <div>
