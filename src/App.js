e8028459fb06f2b30da8e497149c338096df6823
Banukaiw <banukaiw@gmail.com>
2024-08-04 15:11:33 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import Portfolio from "./components/portfolio/portfolio";
 import Contact from "./components/contact/contact";
 import Uproject from "./components/UProject/uproject";
+import { Index } from "./components/index/Index";
 
 
 function App() {
   return (
     <div className="App">
+      <Index/>
       <Navbar/> 
       <Intro/>
       <Skills/>
--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,23 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./intro.css";
+import Preloader from '../preloader/Preloader';
+
 //import ProfileImg from "../../images/profile3.png";
 //import ProfileImg2 from "../../images/proba.png";
-///import BagImg from "../../images/bag.png";
+//import GIF from "/public/images/loader3.gif";
 //import { Link } from "react-scroll";
 import Backvideo from "../../images/backvideo.mp4"
 
 const Intro = () => {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate an API call or some asynchronous operation
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000); // Adjust the timeout duration as needed
+  }, []);
+
+/*   useEffect(() => {
+    const handleLoad = () => {
+        setLoading(false);
+    };
+
+    window.addEventListener('load', handleLoad);
+
+    return () => {
+        window.removeEventListener('load', handleLoad);
+    };
+}, []); */
+
+/*   var loader = document.getElementById("preloader");
+  window.addEventListener("load", function(){
+    loader.style.display = "none";
+  }) */
+
   return (
+    <div>
+       
+
+    <div>
+      {loading ? <Preloader /> : <h1></h1>}
+    </div>
+    
+      
+      
     <section id="intro">
       {/*   <video autoPlay loop className="back-video" muted playsInline>
         <source src="/images/backvideo.mp4" type="video/mp4"/>
       </video>  */}
-
+      
           <video className="video" src={Backvideo} autoPlay loop muted></video>
 
       <div className="container">
 
-        
+      
   
        <div className="intro">
         
@@ -52,6 +90,7 @@ const Intro = () => {
         
       </div>
     </section>
+    </div>
   );
 };
 
